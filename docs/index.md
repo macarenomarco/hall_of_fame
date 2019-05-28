e18e5f12b0a1c50126f55da0c141554f8c7f4c81
Marco Macareno <macareno.marco@gmail.com>
2019-05-27 23:19:44 -0700

    OMI-2 POC Uso de Visualization Query Language
    Summary:
    
    Se implementó el uso del API Visualization Query Language para poder
    obtener los resultados filtrados por olimpiada.
    
    Test Plan:
    
    Ingresar a la pantalla principal y validar que se muestren los datos
    para las distintas hojas de estilo

--- docs/index.md
+++ docs/index.md
@@ -14,5 +14,6 @@ TODO:
    1. Simplifica altamente el proceso de probar localmente cualquier cambio
 2. Seguir pasos en configuración https://developers.google.com/sheets/api/quickstart/js para habilitar el uso del API de Google Spreadsheets
    1. Habrá que tomar en cuenta que este acceso probablamente lo deba dar un administrador de UABC
+   2. Agregar información sobre como asegurar la llave de API para evitar su mal uso
 3. Seguir pasos para configuración de github pages en: https://pages.github.com/ utilizando Organization site (o quizá proyecto, habrá que validar eso)
    1. Deberá ser sin tema, para evitar todo el overheader instalación de Jekyll, y aprender como configurarlo
--- index.js
+++ index.js
@@ -1,33 +1,118 @@
-var spreadSheetId = '1beCDG5iP6lyAQLXshzF7mNAl1MeazsI1NEaWBDxjwrg';
-var API_KEY = 'AIzaSyBRlQRP-s64D_OTDPvBJVeudRH0On0L66w';
-var range = 'Preparatoria!A1:J25';
+var HallOfFameApi = function (apiKey, spreadSheetId) {
+    var getData = function (range) {
+        return fetch(
+            'https://sheets.googleapis.com/v4/spreadsheets/' + 
+            spreadSheetId + 
+            '/values/' + 
+            range + 
+            '?key='+encodeURIComponent(apiKey)
+        ).then(function (response) {
+            if (response.status === 200) {
+                return response.json();
+            }
+        });
+    };
 
-var table = document.getElementById('resultados');
-
-fetch('https://sheets.googleapis.com/v4/spreadsheets/' + spreadSheetId + '/values/' + range + '?key=' + encodeURIComponent(API_KEY))
-    .then(function (response) {
-        response.json()
-            .then(function (jsonResponse) {
-                jsonResponse.values.forEach(function (row, i) {
-                    var isHeader = false;
-                    if (i === 0) {
-                        isHeader = true;
-                    }
-
-                    var cellTag = 'td';
-                    if (isHeader) {
-                        cellTag = 'th';
-                    }
-
-                    var rowTag = document.createElement('tr');
-
-                    row.forEach(function (cellContents) {
-                        var cell = document.createElement(cellTag);
-                        cell.innerText = cellContents;
-                        rowTag.appendChild(cell);
-                    });
-
-                    table.appendChild(rowTag);
-                });
+    this.getOlimpiadas = function () {
+        return getData('Olimpiadas!A:A')
+            .then(function (response) {
+                return response.values;
+            })
+            .then(function (rows) {
+                return rows.map(function (row) {return row[0]});
             });
-    })
+    };
+};
+
+var getEnv = function (defaultEnv) {
+    return fetch('dev.env.json')
+        .then(function (resp) {
+            if (resp.status === 200) {
+                return resp.json();
+            }
+            
+            return defaultEnv;
+        });
+};
+
+
+var API_KEY = 'AIzaSyBRlQRP-s64D_OTDPvBJVeudRH0On0L66w';
+var table = document.getElementById('resultados');
+var tableHeader = table.querySelector('thead');
+var tableBody = table.querySelector('tbody');
+
+var olimpiadaSelect = document.getElementById('olimpiadas')
+
+var spreadSheetId = '1aTG1lp__BLsUfchFs1CJkWRVUDyswXonNXq_TRKgjmk';
+
+google.charts.load('current');
+google.charts.setOnLoadCallback(function () {
+    getEnv({
+        GOOGLE_API_KEY: API_KEY,
+    }).then(function (env) {
+        var apiKey = env.GOOGLE_API_KEY;
+        var api = new HallOfFameApi(apiKey, spreadSheetId);
+        var cache = {};
+
+        function uiRefreshOlimpiadaResults(rawData) {
+            tableHeader.innerHTML = '';
+            tableBody.innerHTML = '';
+
+            var headerRow = document.createElement('tr');
+            rawData.cols.forEach(function (colModel) {
+                var th = document.createElement('th');
+                th.innerText = colModel.label;
+                headerRow.appendChild(th);
+            });
+            tableHeader.appendChild(headerRow);
+
+            rawData.rows.forEach(function (rowData) {
+                var row = document.createElement('tr');
+
+                rowData.c.forEach(function (cellData) {
+                    var td = document.createElement('td');
+                    td.innerText = cellData.v;
+
+                    row.appendChild(td);
+                });
+
+                tableBody.appendChild(row);
+            });
+        }
+
+        function refreshOlimpiadaResults() {
+            var currentOlimpiada = olimpiadaSelect.options[olimpiadaSelect.selectedIndex].value;
+            var query = 'select A, B, C, D, H WHERE A = "' + currentOlimpiada + '"';
+
+            if (cache.hasOwnProperty(query)) {
+                return uiRefreshOlimpiadaResults(cache[query]);
+            }
+
+            var queryObj = new google.visualization.Query(
+                'https://docs.google.com/spreadsheets/d/'+spreadSheetId+'/gviz/tq?sheet=Resultados&headers=1&tq=' + encodeURIComponent(query));
+            queryObj.send(function(response) {
+                var rawData =  JSON.parse(response.getDataTable().toJSON());
+
+                cache[query] = rawData;
+                
+                return uiRefreshOlimpiadaResults(rawData);
+            });
+        }
+
+        olimpiadaSelect.addEventListener('change', function () {
+            refreshOlimpiadaResults();
+        });
+
+        api.getOlimpiadas()
+            .then(function(olimpiadas) {
+                olimpiadas.forEach(function (olimpiadaName) {
+                    var option = document.createElement('option');
+                    option.innerText = olimpiadaName;
+                    option.value = olimpiadaName;
+
+                    olimpiadaSelect.appendChild(option);
+                });
+            })
+            .then(refreshOlimpiadaResults);
+    });
+});
